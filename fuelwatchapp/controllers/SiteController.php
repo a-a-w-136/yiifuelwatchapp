59e5cc2e172c5e968e562fee513ea8f9de6b2271
Adam <adamwoodlee136@hotmail.com>
2019-03-21 15:07:58 +1100

    Controller Processing
    There were some functions defined in the models folder that should have been functionality associated with the controller. They have been moved and the models ahve been deleted.

--- fuelwatchapp/controllers/SiteController.php
+++ fuelwatchapp/controllers/SiteController.php
@@ -12,26 +12,29 @@ use app\models\ContactForm;
 
 class SiteController extends Controller
 {
+    private $log = "/var/www/yiifuelwatchapp/log/fuelwatchapp.log";
+    public $user;
+    public $register_status;
+    public $login_status;
+    public $profile_status;
+
     public function actionIndex() {
-        $user_request = new \app\models\UserRequest;
-        $user_request->processuserrequest();
-        return $this->render('search', ['user' => $user_request->user]);
+        $this->processuserrequest();
+        return $this->render('search', ['user' => $this->user]);
     }
     public function actionRegister() {
-        $user_request = new \app\models\UserRegisterRequest;
-        $user_request->processuserregisterrequest();
-        if($user_request->user == null) {
-            return $this->render('register', ['register_status' => $user_request->register_status]);
+        $this->processuserregisterrequest();
+        if($this->user == null) {
+            return $this->render('register', ['register_status' => $this->register_status]);
         }
         else {
             $this->redirect('index');
         }
     }
     public function actionLogin() {
-        $user_login = new \app\models\UserLoginRequest;
-        $user_login->processuserloginrequest();
-        if($user_login->user == null) {
-            return $this->render('login', ['login_status' => $user_login->login_status]);
+        $this->processuserloginrequest();
+        if($this->user == null) {
+            return $this->render('login', ['login_status' => $this->login_status]);
         }
         else {
             $this->redirect('index');
@@ -43,13 +46,12 @@ class SiteController extends Controller
         $this->redirect('index');
     }
     public function actionProfile() {
-        $user_request = new \app\models\UserProfileRequest;
-        $user_request->processuserprofilerequest();
-        if($user_request->user == null) {
+        $this->processuserprofilerequest();
+        if($this->user == null) {
             $this->redirect('index');
         }
         else {
-            return $this->render('profile', ['user' => $user_request->user]);
+            return $this->render('profile', ['user' => $this->user]);
         }
     }
     public function actionUnregister() {
@@ -59,4 +61,93 @@ class SiteController extends Controller
         }
         $this->redirect('index');
     }
+    function processuserrequest() {
+        try {
+            if($_SERVER["REQUEST_METHOD"] == "GET" && !isset($_SESSION['username'])) {
+                $this->user = new \app\models\FWAUser;
+                $this->user->homepageget($this->log);
+                $this->user->cheapest_fuel = true;
+            }
+            else if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $this->user = new \app\models\FWAUser;
+                $this->user->homepagepost($this->log);
+            }
+            else if(isset($_SESSION['username']) && $_SERVER["REQUEST_METHOD"] == "GET") {
+                $this->user = new \app\models\FWARegisteredUser;
+                $favourite_status = $this->user->profileget($this->log);
+                if(!($this->user->favourite())) {
+                    $this->user->cheapest_fuel = true;
+                }
+                else {
+                    $this->user->favourite = true;
+                }
+            }
+        }
+        catch (Exception $e) {
+            header("Location: index.php"); 
+            exit;
+        }
+    }
+
+    function processuserregisterrequest() {
+           try {
+                if($_SERVER['REQUEST_METHOD'] == "POST") {
+                    $user = new \app\models\FWAUser;
+                    $this->register_status = $user->register($this->log);
+                    if(isset($_SESSION['username'])) {
+                        $this->user = new \app\models\FWARegisteredUser;
+                        $this->user->profileget($this->log);
+                    }
+                }
+               else {
+                   $user = null;
+               }
+            }
+            catch (Exception $e) {
+                exit;
+            }
+    }
+
+    function processuserloginrequest() {
+       try {
+            if($_SERVER['REQUEST_METHOD'] == "POST") {
+                $this->user = new \app\models\FWAUser;
+                $this->login_status = $this->user->login($this->log);
+                if(!(isset($_SESSION['username']))) {
+                    $this->user = null;
+                }
+                else {
+                    $this->user = new \app\models\FWARegisteredUser($this->log);
+                    $this->user->profileget($this->log);
+                }
+            }
+            else {
+                $this->user = null;
+            }
+        }
+        catch (Exception $e) {
+            exit;
+        }
+    }
+    
+    function processuserprofilerequest() {
+           try {
+                if(!isset($_SESSION['username'])) {
+                    $this->user = null;
+                }
+                else if($_SERVER['REQUEST_METHOD'] == "POST"){
+                    $this->user = new \app\models\FWARegisteredUser;
+                    $this->profile_status = $this->user->profilepost($this->log);
+                }
+                else {
+                    $this->user = new \app\models\FWARegisteredUser;
+                    $this->profile_status = $this->user->profileget($this->log);
+                }
+            }
+            catch (Exception $e) {
+                exit;
+            }
+    }
+    
+    
 }
